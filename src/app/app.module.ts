bd44438d0d23500897caf179405853f7c02f04e5
shajn786 <shajn786@gmail.com>
2022-11-25 12:30:32 +0530

    view all todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoAddComponent } from './to-do-add/to-do-add.component';
+import { ToDoViewAllComponent } from './to-do-view-all/to-do-view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoAddComponent
+    ToDoAddComponent,
+    ToDoViewAllComponent
   ],
   imports: [
     BrowserModule,
